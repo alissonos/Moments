59d3dd00df452a3ea49604cbdd9b594a4e95109b
alissonos <alissonoliveirasilva.oliveira@gmail.com>
2022-10-22 11:18:49 -0300

    Finalizando sistema de mensagens

--- src/app/components/pages/new-moment/new-moment.component.ts
+++ src/app/components/pages/new-moment/new-moment.component.ts
@@ -1,8 +1,10 @@
-import { formatDate } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
 import { Moment } from '../../moment';
 
 import { MomentService } from 'src/app/services/moment.service';
+import { MessagesService } from 'src/app/services/messages.service';
 
 @Component({
   selector: 'app-new-moment',
@@ -12,7 +14,11 @@ import { MomentService } from 'src/app/services/moment.service';
 export class NewMomentComponent implements OnInit {
   btnText = 'Compartilhar';
 
-  constructor(private momentService: MomentService) {}
+  constructor(
+    private momentService: MomentService,
+    private messagesService: MessagesService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {}
 
@@ -32,7 +38,9 @@ export class NewMomentComponent implements OnInit {
     await this.momentService.createMoment(formData).subscribe();
 
     // exibir msg
+    this.messagesService.add('Momento adicionado com sucesso!');
 
     // redirect
+    this.router.navigate(['/'])
   }
 }
